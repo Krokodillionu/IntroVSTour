889bf9bdf604ca7c8370dfd31a8adff8261b231d
Opilane TTHK <opilane@tthk.ee>
2025-10-20 09:13:45 +0300

    e

---
+++ Tsyklid/while/While/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.WriteLine("Joonistan sulle kolmnurga:");
+int kolmnurgasuurus = int.Parse(Console.ReadLine());
+
+int i = 1;
+while (i <= kolmnurgasuurus)
+{
+    Console.WriteLine();
+    int j = 1;
+    while (j <= 1)
+    {
+        Console.Write("_░");
+        j++;
+    }
+    i++;
+
+}
