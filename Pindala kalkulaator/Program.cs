d6eccae070efa0307efadb9ec49935ad8c487e6f
Opilane TTHK <opilane@tthk.ee>
2025-10-24 16:02:37 +0300

    e

---
+++ Pindala kalkulaator/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.WriteLine("Tere, kas su tuba on kolmnurga, ringi või ristküliku kujuline?");
+string valik = Console.ReadLine();
+if (valik == "kolmnurk")
+{
+    Console.WriteLine("Palun sisesta oma toa esimese külje andmed (a)");
+    string külgA = Console.ReadLine();
+
+
+}
