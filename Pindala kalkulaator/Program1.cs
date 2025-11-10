3aa93338b1cb8d0adf1bb405c6164277c6f271ff
Opilane TTHK <opilane@tthk.ee>
2025-11-10 11:19:10 +0200

    e

---
+++ Pindala kalkulaator/Program1.cs
@@ -0,0 +1,33 @@
+﻿Console.WriteLine("Kas sinu tuba on kolmnurga, ringi või ristküliku kujuline?");
+string valik = Console.ReadLine();
+if (valik == "kolmnurga")
+{
+    Console.WriteLine("Sisesta oma toa esimese külje mõõdud");
+    double mõõtA = double.Parse(Console.ReadLine());
+    Console.WriteLine("Sisesta oma toa teise külje mõõdud");
+    double mõõtB = double.Parse(Console.ReadLine());
+    Console.WriteLine("Sisesta oma toa kolmanda külje mõõdud");
+    double mõõtC = double.Parse(Console.ReadLine());
+    double kogupindala = mõõtA + mõõtB + mõõtC / 2;
+    Console.WriteLine($"Sinu toa suurus on {kogupindala} kuupmeetrit");
+
+}
+else if (valik == "ristküliku")
+{
+    Console.WriteLine("Sisesta oma toa lühemate külgede pikkused");
+    double lühemkülgA = double.Parse(Console.ReadLine());
+    
+    Console.WriteLine("Sisesta oma toa pikemate külgede pikkused");
+    double pikemkülgA = double.Parse(Console.ReadLine());
+    
+    double kogupindala = (lühemkülgA * lühemkülgA) + ( pikemkülgA * pikemkülgA) ;
+    Console.WriteLine($"Sinu toa kogumõõt on {kogupindala} meetrit");
+    
+}
+else if (valik == "ringi")
+{
+    Console.WriteLine("Sisesta oma läbimõõt");
+    double raadius = double.Parse(Console.ReadLine());
+    double ringipindala = (raadius * raadius) * Math.Pi; 
+    Console.WriteLine($"Sinu toa läbimõõt on {ringipindala} m²");
+}
---
+++ Tsyklid1/Kordamisülesanded/TsükliteKordusÜlesanded/Program.cs
@@ -0,0 +1,49 @@
+﻿//Iseseisev töö 
+//Tsüklite kontrolltööks ettevalmistavad ülesanded:
+// 1. külalised sünnipäeval
+/* dowhile
+küsi kasutajalt tema eesnime, tsükkel peab toimima niikaua kuni kasutaja on sisestanud midagi, et sisestus tühi "" ei oleks.
+*/
+Console.WriteLine("Palun sisesta oma eesnimi");
+string nimi = "";
+do
+{
+    Console.WriteLine("sinu nimi on:");
+    nimi = Console.ReadLine();
+} while (nimi == "");
+
+
+/* while
+siis küsi kasutajalt tema sünnikuupäeva kolme while tsükliga, kõigepealt päev, arvuna, siis kuu, nimena, ning siis aasta, arvuna
+päeva küsimise juures tuleb jälgida et kuupäev oleks vahemikus 1-31,
+kuu küsimise juures peab uuesti küsima, kui nimetus ei esine programmis (kas switch-case, if-elseif, või .Contains() abiga)
+aasta juures ei tohi olla sünniaasta 19ndas sajandis (18xx) ega tulevikus. (tekib ka vahemik)
+*/
+Console.WriteLine("Mis päeval on su sünnipäev?");
+string sünnipäev = "";
+sünnipäev = Console.ReadLine();
+
+{
+    Console.WriteLine("sinu sünnipäev on:");
+    
+
+} while (sünnipäev
+
+
+/* for
+küsi kasutajalt tema sünnipäevale küllatulijaid uude listi for tsükliga, iga tsükkel sisestab uue inimesenime, kuni kasutaja ütleb "kõik"
+*/
+/* foreach
+küsi kasutajalt kas on mõni külaline keda ta siiski näha ei tahaks? ning võrdle foreach tsüklis kõik külalised kasutaja sisestatuga.
+eemalda see külaline kui ta nimekirjas on, kui külalist nimekirjast ei leita, siis ütle "sellist inimest sa pole kutsunudki"
+*/
+ 
+ 
+ 
+/* 2. Üldine ülesanne
+kirjuta programm mis küsib kasutajalt tema lemmikvärvi, ning joonistab talle tema lemmimkvärvi ruudu, mille sisse kirjutad vastava värvi ainult keskmisele reale. Kasutada võib vabalt valitud tsüklitüüpi
+*/
+
+
+    
+
