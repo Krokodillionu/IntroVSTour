8a3e3f7c6061150039af68016a1b860a85f747b1
Opilane TTHK <opilane@tthk.ee>
2025-11-24 09:35:53 +0200

    e

---
+++ Tsyklid1/MeetodJaCastimine/Meetod2/Meetod2/Program.cs
@@ -0,0 +1,44 @@
+﻿namespace Meetod2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Tervitus();
+            string kasutajanimi = "";
+            kasutajanimi = NimeKüsimine(kasutajanimi);
+            {
+                Tervitus();
+                Console.WriteLine("Palun sisesta oma kasutajanimi");
+                kasutajanimi = Console.ReadLine();
+            }
+            Console.WriteLine("Mis on sinu nädalane eelarve?");
+            float eelarve = 0.00f;
+            bool kaskasutajanõustub = false;
+            while (kaskasutajanõustub!= true)
+            {
+                Console.WriteLine($"Praegune eelarve on {eelarve}, kas see on õige");
+                Console.WriteLine("Vasta, kas jah või ei");
+                string mistaütles = Console.ReadLine();
+                if (mistaütles == "jah")
+                {
+                    kaskasutajanõustub = true;
+                }
+                Console.WriteLine($"{kasutajanimi}sinu eelarve on {SinuEelarvePäevas(eelarve)}");
+                SinuEelarvePäevas(eelarve);
+            }
+
+           
+        }
+        public static void Tervitus()
+        {
+            Console.WriteLine("Hommik");
+        }
+
+        public static int SinuEelarvePäevas(float eelarve)
+        {
+            double eelarvepäevas = eelarve / 7;
+            return (int)eelarvepäevas;
+        }
+    }
+}
---
+++ Tsyklid1/Töö/ConsoleApp1/Program.cs
@@ -0,0 +1,27 @@
+﻿using System.Globalization;
+
+Console.WriteLine("Mis on sinu nimi?");
+string nimi = "";
+do
+{
+    nimi = Console.ReadLine();
+    Console.WriteLine($"Tere, {nimi}");
+} while (nimi != "");
+
+if (nimi == "")
+{
+    Console.WriteLine("Tundmatu sisestus, palun sisesta tuntav nimi");
+}
+Console.WriteLine("Mis on sinu parool? Parool peab olema neljakohaline");
+int parool = 0;
+{
+    parool = int.Parse(Console.ReadLine());
+    Console.WriteLine($"Sinu parool {parool} on õige, olete sisse lubatud");
+
+} while (parool >= 1000 && parool < 9999) ;
+
+if (parool > 9999 && parool < 999)
+{
+    Console.WriteLine("Parool pole neljakohaline, vale parool");
+}
+
