fedf05aedb9d45f6d9f288ee326a20eaead4a8e7
Opilane TTHK <opilane@tthk.ee>
2025-12-08 11:36:39 +0200

    e

---
+++ Tsyklid1/Kiirtoidu tellija programm/Kiirtoidu tellija/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Kiirtoidu_tellija
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
---
+++ Tsyklid1/Ostukorv/Ostukorvi/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace Ostukorvi;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        bool islogin;
+        string kasutajanimi = "";
+        string parool = "";
+        Console.WriteLine("Mis on sinu kasutajanimi?");
+        kasutajanimi = Console.ReadLine();
+        
+        if (kasutajanimi == "Krokodillionu")
+        {
+            Console.WriteLine($"Tere, {kasutajanimi}, palun sisesta oma parool");
+        }
+        while (kasutajanimi != "Krokodillionu")
+        {
+            Console.WriteLine("Tundmatu sisestus, ei tunne sellist inimest");
+        }
+        parool = Console.ReadLine();
+        if (parool == "Kassike100")
+        {
+            Console.WriteLine("Õige parool");
+        }
+        while (parool != "Kassike100")
+        {
+            Console.WriteLine("Vale parool, palun sisesta õige parool!");
+        }
+
+    }
+}
